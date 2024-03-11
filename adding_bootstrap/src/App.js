e4253824fd16dfdabbda0d0890d808c297e3b2b9
sandeshapokhrel <8sejal7@gmail.com>
2024-03-11 22:13:28 +0545

    add productlist

--- adding_bootstrap/src/App.js
+++ adding_bootstrap/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
 
 function App() {
+  const productlist= [
+    {
+      price: 100000,
+      name:"Iphone 12 Max",
+      quantity: 0,
+    },
+    {
+      price: 20000,
+      name:"Samsung 12 Max",
+      quantity: 0,
+    }
+  ]
   return (
     <div>
       <Navbar/>
+      <ProductList productlist={productlist}/> 
     </div>
   );
 }
---
+++ adding_bootstrap/src/Components/ProductList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Product from './Product';
+//there are two ways to take an argunment
+// 1.destructring object
+// 2. props
+//props and variable are two different things
+//props are not changable 
+//All components on react are pure function where pure function mean The output remains the same as long as the inputs remain unchanged
+//Using Destructure:- 
+// export default function ProductList({product}) { 
+  //  console.log(product)
+// Using props:-
+//for loop can be use in jsx we need to use map finction, foreach 
+export default function ProductList (props) {
+  return (
+    props.productlist.map((product)=>{
+        return <Product product={product} />
+    })
+  )
+}
